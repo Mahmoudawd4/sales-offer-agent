c81263aac37e2f3ebfae7221b77e468385230715
Mahmoud Awd <38701340+Mahmoudawd4@users.noreply.github.com>
2026-04-09 10:24:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ PROJECTS_DATABASE = {
     },
     "KHALIFA CITY": {
         "url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSLDSBkzA1ZpD1qCRFjl4TiNWldYobalUdgwADyljTFkWMJrvVXajgFxegKWDr2SA-UcuAc8mGonW36/pub?gid=1491192679&single=true&output=csv",
-        "gov_pct": 2.0,
+        "gov_pct": 1.0,
         "admin_fees": 625,
         "res_fee": 20000
     },
