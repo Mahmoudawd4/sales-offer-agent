4d360d2fedd12b880e79693f0ad6030bd749ec9e
Mahmoud Awd <38701340+Mahmoudawd4@users.noreply.github.com>
2026-04-16 14:50:43 +0400

    Update app.py

--- app.py
+++ app.py
@@ -317,7 +317,13 @@ PROJECTS_DATABASE = {
         "admin_fees": 1194,
         "res_fee": 20000 
     }
-
+,
+    "VERDANA 6 R": {
+        "url":"https://docs.google.com/spreadsheets/d/e/2PACX-1vSLDSBkzA1ZpD1qCRFjl4TiNWldYobalUdgwADyljTFkWMJrvVXajgFxegKWDr2SA-UcuAc8mGonW36/pub?gid=290574832&single=true&output=csv",
+        "gov_pct": 4.0,
+        "admin_fees": 1194,
+        "res_fee": 20000 
+    }
 
 }
 
