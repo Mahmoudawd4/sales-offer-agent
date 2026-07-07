f76d3a1b5adc30a9f2a908223a0eeaca8443b178
Mahmoud Awd <38701340+Mahmoudawd4@users.noreply.github.com>
2026-07-07 13:25:36 +0400

    Update app.py

--- app.py
+++ app.py
@@ -154,6 +154,28 @@ PROJECTS_DATABASE = {
 
     }
     ,
+ "Reportage Oceana": {
+
+
+
+        "url":"https://docs.google.com/spreadsheets/d/e/2PACX-1vSLDSBkzA1ZpD1qCRFjl4TiNWldYobalUdgwADyljTFkWMJrvVXajgFxegKWDr2SA-UcuAc8mGonW36/pub?gid=1449190315&single=true&output=csv",
+
+
+
+        "gov_pct": 2.5,
+
+
+
+        "admin_fees": 5350,
+
+
+
+        "res_fee": 20000 
+
+
+
+    }
+    ,
 
 
 
